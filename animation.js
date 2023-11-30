e2dcf897a1c6e576c5b97e3c061d59a52d94b8dd
esteegoel <109704190+esteegoel@users.noreply.github.com>
2023-11-30 13:25:12 -0500

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,11 +1,8 @@
  // update the position of boat based on scroll
  function updateBoatPosition() {
     const scrollY = window.scrollY || window.pageYOffset;
-    const translateY = scrollY / 1.5; // from w3schools
-    const minTop = 10; 
-    const maxTop = 80;
-    const minBottom = 10; 
-    const maxBottom = 80;
+    const translateY = scrollY / 1; // from w3schools
+    
 
     document.querySelector('.overlay').style.top = `calc(50% + ${translateY}px)`;
 }
