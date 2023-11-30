814f4c6182f42b06f8dde43c6a15c2451c0a0406
esteegoel <109704190+esteegoel@users.noreply.github.com>
2023-11-30 14:04:01 -0500

    animation adjustments

--- animation.js
+++ animation.js
@@ -2,7 +2,7 @@
  function updateBoatPosition() {
     const scrollY = window.scrollY || window.pageYOffset;
     const translateY = scrollY / 1; // from w3schools
-    const minTop = 100;
+    const minTop = 60;
 
     document.querySelector('.overlay').style.top = `calc(50% + ${translateY}px)`;
 }
