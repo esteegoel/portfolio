86485385037f15f986e1ce2451531e681e19b5c7
esteegoel <109704190+esteegoel@users.noreply.github.com>
2023-12-02 17:55:43 -0500

    animation editing

--- animation.js
+++ animation.js
@@ -1,13 +1,20 @@
  // update the position of boat based on scroll
  function updateBoatPosition() {
     const scrollY = window.scrollY || window.pageYOffset;
-    const translateY = scrollY / 1; // from w3schools
+    const translateY = scrollY / 1;
     const minTop = 60;
 
-    document.querySelector('.overlay').style.top = `calc(50% + ${translateY}px)`;
+    // calculate max top value from viewport height
+    const maxTop = window.innerHeight * 0.75;
+
+    // set  top value within  range of minTop and maxTop
+    const finalTop = Math.min(maxTop, Math.max(minTop, translateY));
+
+    document.querySelector('.overlay').style.top = `calc(50% + ${finalTop}px)`;
 }
 
-// listen for the scroll event and call  update function
+// listen for the scroll event and call update function
+window.addEventListener('load', updateBoatPosition);
 window.addEventListener('scroll', updateBoatPosition);
 
 updateBoatPosition();
