02cba63321776f87738690ed1f480a6416d00bab
esteegoel <109704190+esteegoel@users.noreply.github.com>
2023-12-06 16:30:12 -0500

    boat function

---
+++ animation.js
@@ -0,0 +1,19 @@
+ function updateBoatPosition() {
+    const scrollY = window.scrollY || window.pageYOffset;
+    const translateY = scrollY / 1;
+    const minTop = 60;
+
+    // calculate max top value from viewport height
+    const maxTop = window.innerHeight * 0.75;
+
+    // set  top value in range
+    const finalTop = Math.min(maxTop, Math.max(minTop, translateY));
+
+    document.querySelector('.overlay').style.top = `calc(50% + ${finalTop}px)`;
+}
+
+// listen for scroll event and call update function
+window.addEventListener('load', updateBoatPosition);
+window.addEventListener('scroll', updateBoatPosition);
+
+updateBoatPosition();
